ccd1e824e7215674436e011ac8cc7f79bfe5cf3b
Roomsie Jones <seangravito@gmail.com>
2021-07-14 12:13:04 -0500

    Added the server paths and left the actual sendfiles in comments, need to update the filenames at a later time as well, to match the HTML files. Opened in browser, console logs worked, should be functional.

--- server.js
+++ server.js
@@ -3,10 +3,39 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+// this will later be the object to store our tables
+const tables =[];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get ('/', function(req, res){
+    // res.sendFile(path.join(__dirname, "index.html"))
+    console.log('Home Page has been initialized!');
+})
+
+app.get ('/reserve', function(req, res){
+    // res.sendFile(path.join(__dirname, "reserve.html"))
+    console.log('Make a reservation');
+})
+
+app.get ('/tables', function(req, res){
+    // res.sendFile(path.join(__dirname, "viewtables.html"))
+    console.log('View tables');
+})
+
+app.get ('/api/tables', function(req, res) {
+    // res.json(tables);
+    
+    console.log('Tables API call');
+})
+
+app.get ('/api/waitlist', function(req, res){
+    // res.json(waitlist);
+    console.log('Waitlist API call');
+})
+
+
 
 app.listen(PORT, function() {
     console.log(`Listening on PORT ${PORT}`);
